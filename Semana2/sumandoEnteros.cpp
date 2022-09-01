8085e5266b42636d11ae85036656198186110a66
Renato <renato.quispe@ucsp.edu.pe>
2022-09-01 08:20:51 -0500

    Programa nuevo, Sumando Enteros.2

---
+++ Semana2/sumandoEnteros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a = 20
+
+    int b;
+    b = 10
+
+    int suma = a + b; 
+
+    cout << "La suma es: " << suma << endl;
+    return 0; 
+}

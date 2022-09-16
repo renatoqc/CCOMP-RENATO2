0a5df03fc83132f63326d4b4029cce9be9086dd9
Renato <renato.quispe@ucsp.edu.pe>
2022-09-16 07:41:56 -0500

    Quinto cambio

---
+++ semana4/Project-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int n = 1000;
+
+    for (int i = 0; i <n; i++)
+    {
+        if(3*i < n)
+			sum += 3*i;
+		if(5*i < n && (5*i) % 3 != 0)
+			sum += 5*i;
+	}
+	cout << "Sum: " << sum << endl;
+	return 0;
+    
+}
---
+++ semana4/Project-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int aux = 0
+    int fib = 1
+
+    cout << INFO   
+
+}

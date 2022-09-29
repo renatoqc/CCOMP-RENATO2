9dbd59dc88918b6c2988cbd507fa97f47b13dd69
Renato <renato.quispe@ucsp.edu.pe>
2022-09-29 07:29:45 -0500

    Semana6

--- semana4/Project-5.cpp
+++ semana4/Project-5.cpp
@@ -5,17 +5,4 @@ using namespace std;
 
 int main(){
 
-    int to_string;
-    for(long int i = 100; i < 1000; i++)
-    {
-        for(long int j = 100; j < 1000; j++)
-        {
-            long int prod = i*j;
-            cout<<to_string(prod)<<endl;
-        
-        }
-
-        continue;
-    }
-    return 0;
-}
+    
